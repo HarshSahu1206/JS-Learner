dd9d70a19d2e0e635b19bc514e61578e09c1da14
HarshSahu1206 <harshsahu1206@gmail.com>
2026-05-28 18:27:47 +0000

    Day 3 JS

---
+++ 04_ctrlflow/switch.js
@@ -0,0 +1,30 @@
+// Switch is JS
+
+// switch (key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch(month){
+    case 1 :
+        console.log("Januarary");
+        break;
+    case 2 :
+        console.log("Feburary");
+        break;
+    case 3 :
+        console.log("March");
+        break;
+    case 4 :
+        console.log("April");
+        break;
+    default :
+        console.log("Not Found.");
+        
+}
+    
