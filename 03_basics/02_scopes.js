af534b26a5911856f46f5ade0215ab05a94cd765
HarshSahu1206 <harshsahu1206@gmail.com>
2026-05-28 13:56:21 +0000

    Day 3 JS

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -17,4 +17,43 @@ if(true){
 
 //console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website); //website is only accessible inside func two()
+
+     two()
+
+}
+
+//one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+console.log(addone(5))
+
+function addone(num){ // If we make function like this we can call it before
+    return num + 1
+}
+
+
+
+//addTwo(5)
+const addTwo = function(num){ // but like this we can't call before initilization
+    return num + 2
+}
