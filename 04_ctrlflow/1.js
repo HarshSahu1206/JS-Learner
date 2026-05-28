d9c05e68302fc52ed750c0b287e48ade0b4502fa
HarshSahu1206 <harshsahu1206@gmail.com>
2026-05-28 18:22:16 +0000

    Day 3 JS

--- 04_ctrlflow/1.js
+++ 04_ctrlflow/1.js
@@ -2,11 +2,32 @@
 
 //if
 if (2 == "2"){ // <, >, <=, >=, !=, ==, ===, !==
-    console.log("Yes");
+    //console.log("Yes");
     
 }
 
 if (2 === "2"){ 
-    console.log("Yes");
+    //console.log("Yes");    
+}
+
+const score = 200
+
+if (score > 100){
+    const power  = "fly"
+    //console.log(`Your Power is ${power}`);
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = true
+const loggedInFromEmail = !true
+
+
+if(userLoggedIn && debitCard){
+    //console.log("Allowed to buy course");   
+}
+
+if(loggedInFromEmail ||loggedInFromGoogle) {
+    console.log("User logged in successfully.");
     
 }
