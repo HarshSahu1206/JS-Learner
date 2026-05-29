58724f981bc1711ae4b07c26d16578c6e9d0a540
HarshSahu1206 <harshsahu1206@gmail.com>
2026-05-29 21:40:54 +0530

    Day 4 JS

---
+++ 05_iteration/05_.js
@@ -0,0 +1,36 @@
+const coading = ["js", "ruby", "java", "python", "cpp"]
+
+coading.forEach( function (item) {  // Call back fx there fore no name given to it.
+  //  console.log(item);
+    
+})
+
+coading.forEach( (item) => {
+  //  console.log(item);
+    
+})
+
+coading.forEach( (item , index , arr) => {
+  //  console.log(item, index, arr);
+    
+})
+
+const code = [
+    {
+        languageName : "JavaScript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "c++",
+        languageFileName : "cpp"
+    },
+    {
+        languageName : "Python",
+        languageFileName : "py"
+    }
+]
+
+code.forEach( (item) => {
+   // console.log(item.languageName)
+}
+) 
